d588475ba3006875131830e645c92d8fed02e599
Jayanti Hari <lnu.jay@northeastern.edu>
2024-05-24 02:13:51 -0400

    saturation and lightening bar

--- color-picker/main.js
+++ color-picker/main.js
@@ -1,4 +1,18 @@
 let colorWheel = document.getElementById("colorWheel");
+const rect = colorWheel.getBoundingClientRect();
+
+console.log(rect.width);
+let saturationBar = document.querySelector(".saturation");
+saturationBar.style.width = `${rect.width}px`;
+
+let lighteningBar = document.querySelector(".lightening");
+lighteningBar.style.width = `${rect.width}px`;
+
+// const hslColor = {
+//   hue: 120,
+//   saturation: 100,
+//   lightness: 50,
+// };
 
 let arr = [];
 
@@ -21,6 +35,15 @@ colorWheel.addEventListener("click", (event) => {
   const angle = Math.atan2(y, x) * (180 / Math.PI);
   const hue = angle + 90;
 
+  const currentColor = `hsl(${hue}, 100%, 50%)`;
+  const currentColor0Sat = `hsl(${hue}, 0%, 50%)`;
+
   const colorInputDiv = document.getElementById("pickerColorDiv");
-  colorInputDiv.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
+  colorInputDiv.style.backgroundColor = currentColor;
+
+  saturationBar.style.backgroundColor = currentColor;
+  saturationBar.style.backgroundImage = `linear-gradient(90deg, ${currentColor0Sat}, ${currentColor})`;
+
+  lighteningBar.style.backgroundColor = currentColor;
+  lighteningBar.style.backgroundImage = `linear-gradient(90deg, )`;
 });
